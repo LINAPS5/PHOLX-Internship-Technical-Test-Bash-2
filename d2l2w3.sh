0754338368fab8da39716e925162a30315076ff2
LINAPS5 <linapsmsi@gmail.com>
2026-04-07 16:34:44 +0700

    Refactor awk command for readability

--- d2l2w3.sh
+++ d2l2w3.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-awk 'tolower($0)~/error/{
-	x=$5
-	sub(/\[.*\]:/,"",x)
-	count[x]++
-}
-END{
-	for (s in count)print count[s],s
-}' syslog_test.log
+awk 'tolower($0)~/error/{x=$5;sub(/\[.*\]:/,"",x);count[x]++}END{for (s in count)print count[s],s}' syslog_test.log
